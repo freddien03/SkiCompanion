fd193a6d35d24737d531909a5b4222683b394e38
freddien03 <freddienunn@gmail.com>
2021-11-01 11:24:59 +0000

    added firestorm user reading

---
+++ SkiCompanion/FirestoreManager.swift
@@ -0,0 +1,13 @@
+//
+//  FirestoreManager.swift
+//  SkiCompanion
+//
+//  Created by Nunn, Frederick (IRG) on 01/11/2021.
+//
+
+import Foundation
+import Firebase
+
+class FirestoreManager: ObservableObject {
+    
+}
--- SkiCompanion/StateController.swift
+++ SkiCompanion/StateController.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import CoreLocation
+import Firebase
 
 class StateController: ObservableObject {
     let locationHandler: LocationHandler = LocationHandler()
@@ -16,12 +17,45 @@ class StateController: ObservableObject {
     var UserID: String = "" {
         didSet {
             print(UserID)
+            print(fetchUser(ID: UserID))
         }
     }
     
     init(currentResort: String, currentUser: User) {
         self.currentResort = currentResort
         self.currentUser = currentUser
+    }
+    
+    func fetchUser(ID: String) {
+        var userData : [String:String] = [:]
+        let db = Firestore.firestore()
+        let docRef = db.collection("users").document(ID)
+        
+        docRef.getDocument { (document, error) in
+            guard error == nil else {
+                print("error", error ?? "")
+                return
+            }
+            
+            if let document = document, document.exists {
+                let data = document.data()
+                if let data = data {
+                    userData = data
+                }
+            }
+        }
+        
+        db.collection("users").document("ID").collection("Achievements").getDocuments() { (querySnapshot, error) in
+            if let error = error {
+                    print("Error getting documents: \(error)")
+            } else {
+                    for document in querySnapshot!.documents {
+                            print("\(document.documentID): \(document.data())")
+                    }
+            }
+        }
+        
+        self.currentUser = User(email: userData["email"]!, password: userData["password"]!, currentResort: userData["currentResort"]!, achievements: [] )
     }
     
 }
